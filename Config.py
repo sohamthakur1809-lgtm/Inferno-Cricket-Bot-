8d9c07b2e1c63287367cf6cdf254e5af84c6c93f
sohamthakur1809-lgtm <sohamthakur1809@gmail.com>
2026-05-23 17:21:48 +0530

    Config.py

--- Config.py
+++ Config.py
@@ -1,4 +1,4 @@
 import os
 
-BOT_TOKEN = os.getenv("8994887028:AAHPml65uq8D6bZk26gl-UfjU4OlhMS3nVo")
-OWNER_ID = int(os.getenv("5533367541"))
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID"))
